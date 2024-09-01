805799ef460596d52b5ee6caa39b43d8d43c64d0
Gianni Schiralli <gschiralli@myseneca.ca>
2024-09-01 14:13:24 -0400

    Add: [...url] route

---
+++ src/app/[...url]/page.tsx
@@ -0,0 +1,55 @@
+import { ChatWrapper } from "@/components/ChatWrapper";
+import { ragChat } from "@/lib/rag-chat";
+import { redis } from "@/lib/redis";
+import { cookies } from "next/headers";
+
+interface PageProps {
+  params: {
+    url: string | string[] | undefined;
+  };
+}
+
+function reconstructUrl({ url }: { url: string[] }) {
+  const decodededComponents = url.map((component) =>
+    decodeURIComponent(component)
+  );
+
+  return decodededComponents.join("/");
+}
+
+const Page = async ({ params }: PageProps) => {
+  const sessionCookie = cookies().get("sessionId")?.value;
+  const reconstructedUrl = reconstructUrl({ url: params.url as string[] });
+
+  const sessionId = (reconstructedUrl + "--" + sessionCookie).replace(
+    /\//g,
+    ""
+  );
+
+  const isAlreadyIndexed = await redis.sismember(
+    "indexed-urls",
+    reconstructedUrl
+  );
+
+  const initialMessages = await ragChat.history.getMessages({
+    amount: 10,
+    sessionId,
+  });
+
+  if (!isAlreadyIndexed) {
+    //Add to vector db
+    await ragChat.context.add({
+      type: "html",
+      source: reconstructedUrl,
+      config: { chunkOverlap: 50, chunkSize: 200 },
+    });
+    //Cache url in redis db
+    await redis.sadd("indexed-urls", reconstructedUrl);
+  }
+
+  return (
+    <ChatWrapper sessionId={sessionId} initialMessages={initialMessages} />
+  );
+};
+
+export default Page;
